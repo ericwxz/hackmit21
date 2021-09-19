50ada6891f8a767dbf6b85a0f69c3eee4c911ebe
ericwxz <ericwxzhang@gmail.com>
2021-09-18 23:48:55 -0400

    hid secrets and added requirements

--- art_explorer/settings.py
+++ art_explorer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*mf@xk93h%-qw!n=bd_y(&1n@nif-fjys@4bryh2ig4%x_&0&q'
+SECRET_KEY = SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
